3e9612621d5e2aaba5090b9d0be83af71d442700
Philip Mateo Pfeffer <philipmateopfeffer@MacBook-Pro.local>
2020-12-10 13:00:52 -0300

    Added more description, started light mode

--- js/dark_toggle.js
+++ js/dark_toggle.js
@@ -1,4 +1,8 @@
 function dark_button() {
     var body_elem = document.body;
     body_elem.classList.toggle("dark-mode");
+    
+    var header_elem = document.getElementById('header');
+    console.log(header_elem.classList)
+    header_elem.classList.toggle("dark-mode");  
   }
