4e845b6ed21cf785efc86a95d6f34982e6d1ca95
Philip Mateo Pfeffer <philipmateopfeffer@MacBook-Pro.local>
2020-12-10 13:38:09 -0300

    Working dark mode, ugly button

--- js/dark_toggle.js
+++ js/dark_toggle.js
@@ -2,7 +2,9 @@ function dark_button() {
     var body_elem = document.body;
     body_elem.classList.toggle("dark-mode");
     
-    var header_elem = document.getElementById('header');
-    console.log(header_elem.classList)
-    header_elem.classList.toggle("dark-mode");  
+    var elems = document.getElementsByClassName('content');
+    for (var i = 0; i < elems.length; i++) {
+      if (elems[i].style.borderWidth === "1px") elems[i].style.border = "0px solid #000";
+      else elems[i].style.border = "1px solid #333";
+    }
   }
