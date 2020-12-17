824edf1d3ded1471f93c31848205f2d4a4b77483
Philip Mateo Pfeffer <philipmateopfeffer@MacBook-Pro.local>
2020-12-17 15:08:10 -0300

    Added more project shells, westminster forge

--- js/dark_toggle.js
+++ js/dark_toggle.js
@@ -7,4 +7,8 @@ function dark_button() {
       if (elems[i].style.borderWidth === "1px") elems[i].style.border = "0px solid #000";
       else elems[i].style.border = "1px solid #333";
     }
+
+    var forge_uni = document.getElementById("forge_uni");
+    if (forge_uni.getAttribute('src') != "./imgs/forge_unicycle_inverted.png") forge_uni.src = "./imgs/forge_unicycle_inverted.png";
+    else forge_uni.src = "./imgs/forge_unicycle.png";
   }
