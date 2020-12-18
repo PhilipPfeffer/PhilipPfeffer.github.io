d0d6f1edb71970c0718f35347c762f73375161b3
Philip Mateo Pfeffer <philipmateopfeffer@MacBook-Pro.local>
2020-12-18 19:09:43 -0300

    wormhole and jiiff

--- js/dark_toggle.js
+++ js/dark_toggle.js
@@ -8,7 +8,7 @@ function dark_button() {
       else elems[i].style.border = "1px solid #333";
     }
 
-    var forge_uni = document.getElementById("forge_uni");
-    if (forge_uni.getAttribute('src') != "./imgs/forge_unicycle_inverted.png") forge_uni.src = "./imgs/forge_unicycle_inverted.png";
-    else forge_uni.src = "./imgs/forge_unicycle.png";
+    // var forge_uni = document.getElementById("forge_uni");
+    // if (forge_uni.getAttribute('src') != "./imgs/forge_unicycle_inverted.png") forge_uni.src = "./imgs/forge_unicycle_inverted.png";
+    // else forge_uni.src = "./imgs/forge_unicycle.png";
   }
